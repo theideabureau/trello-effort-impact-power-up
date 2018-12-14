b8fdab0c02e87ba73d082922c0d5dae8ed5853a9
Ben Everard <hello@beneverard.co.uk>
2018-12-14 23:40:29 +0000

    Updates

---
+++ js/input.js
@@ -0,0 +1,38 @@
+/* global TrelloPowerUp */
+
+var t = TrelloPowerUp.iframe();
+
+window.estimate.addEventListener('submit', function(event) {
+
+	// Stop the browser trying to submit the form itself.
+	event.preventDefault();
+
+	return t.set('card', 'shared', 'effort-impact', {
+		effort: window.effort.value,
+		impact: window.impact.value
+	})
+	.then(function(){
+		t.closePopup();
+	});
+
+});
+
+
+t.render(function() {
+
+	return t.get('card', 'shared', 'effort-impact')
+	.then(function(data){
+
+		if ( typeof data === 'undefined' ) {
+			return;
+		}
+
+		window.effort.value = data.effort;
+		window.impact.value = data.impact;
+		
+	})
+	.then(function(){
+		t.sizeTo('#estimate').done();
+	});
+
+});
