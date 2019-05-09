4254e2504e6a4626a5b23c880b74a16f13645acc
Ben Everard <hello@beneverard.co.uk>
2019-05-09 23:53:48 +0100

    Improved card front detail output
    Adding settings to enable/disable card front badges

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,7 @@
 /* global TrelloPowerUp */
 
+import {eiSettings} from './functions.js'
+
 var t = TrelloPowerUp.iframe();
 
 window.settings.addEventListener('submit', function(event) {
@@ -7,11 +9,17 @@ window.settings.addEventListener('submit', function(event) {
 	// Stop the browser trying to submit the form itself.
 	event.preventDefault();
 
-	return t.set('board', 'private', 'labels', {
-		1: window.score_1.value,
-		2: window.score_2.value,
-		3: window.score_3.value,
-		4: window.score_4.value
+	return t.set('board', 'private', 'settings', {
+		card_front_display: {
+			effort_impact: window.ei__cf_effort_impact.checked,
+			score: window.ei__cf_score.checked
+		},
+		labels: {
+			1: window.ei__score_1.value,
+			2: window.ei__score_2.value,
+			3: window.ei__score_3.value,
+			4: window.ei__score_4.value
+		}
 	})
 	.then(function(){
 		t.closePopup();
@@ -21,17 +29,18 @@ window.settings.addEventListener('submit', function(event) {
 
 t.render(function() {
 
-	return t.get('board', 'private', 'labels')
-	.then(labels => {
+	return eiSettings(t)
+	.then(settings => {
 
-		if ( typeof labels === 'undefined' ) {
-			return;
-		}
+		// card front display
+		window.ei__cf_effort_impact.checked = settings.card_front_display.effort_impact;
+		window.ei__cf_score.checked = settings.card_front_display.score;
 
-		window.score_1.value = labels[1];
-		window.score_2.value = labels[2];
-		window.score_3.value = labels[3];
-		window.score_4.value = labels[4];
+		// labels
+		window.ei__score_1.value = settings.labels[1];
+		window.ei__score_2.value = settings.labels[2];
+		window.ei__score_3.value = settings.labels[3];
+		window.ei__score_4.value = settings.labels[4];
 
 	})
 	.then(function(){
